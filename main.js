a81268cf930daf1f2ae2a60bbbc1ad9f99ab1248
Sascha Khbabez <sascha.khbabez@outlook.de>
2025-08-03 16:36:57 +0200

    Added reset functionality

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+"use strict";
+const calculator = document.getElementById("calculator");
+const form = document.getElementById("tip-form");
+const tipButtons = calculator.querySelectorAll("input[type='checkbox']");
+const customInput = document.getElementById("input-custom");
+const billInput = document.getElementById("input-bill");
+const peopleInput = document.getElementById("input-people");
+const peopleError = document.getElementById("people-error");
+const billError = document.getElementById("bill-error");
+
 const roundCurrency = (amount) => {
   return Math.round(amount * 100) / 100;
 };
@@ -12,5 +22,12 @@ const calculateBill = (noPeople = 1, bill = 0, tip = 0) => {
   const total = amountPerPerson + tipPerPerson;
   return [floorCurrency(tipPerPerson), roundCurrency(total)];
 };
+
+const resetErrors = () => {
+  billError.textContent = "";
+  peopleError.textContent = "";
+};
+
+form.addEventListener("reset", resetErrors);
 
 console.log(calculateBill(5, 142.55, 0.15));
